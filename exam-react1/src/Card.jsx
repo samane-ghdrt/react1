4a6eca7803466fb1fc4c91a37aef9b456da57e63
samane ghodrati <samane.ghodrati@gmail.com>
2025-05-20 21:07:47 -0900

    add

--- exam-react1/src/Card.jsx
+++ exam-react1/src/Card.jsx
@@ -2,20 +2,20 @@ import apsignals from "./img/dark-mode.be26bfcd.png"
 
 export default function Card (){
     return(
-    <div class="card m-5">
+    <div className="card m-5">
   <img
-    class="card-img-top"
+    className="card-img-top"
     src={apsignals}
     alt="Card image cap"
   />
-  <div class="card-body">
-    <h5 class="card-title">Bob Dylan</h5>
-    <p class="card-text">
+  <div className="card-body">
+    <h5 className="card-title">Bob Dylan</h5>
+    <p className="card-text">
       Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American
       singer/songwriter, author, and artist who has been an influential figure
       in popular music and culture for more than five decades.
     </p>
-    <a href="https://en.wikipedia.org/wiki/Bob_Dylan" class="btn btn-primary">
+    <a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">
       Go to wikipedia
     </a>
   </div>
